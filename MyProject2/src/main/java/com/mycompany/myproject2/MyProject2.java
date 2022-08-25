ff75755307e8873a72cab25c0dcf6222590dcc14
muadz2021 <muadz_2021@student.uns.ac.id>
2022-08-25 10:33:14 +0700

    My Project 1 & My Project 2

---
+++ MyProject2/src/main/java/com/mycompany/myproject2/MyProject2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.myproject2;
+
+/**
+ *
+ * @author DELL
+ */
+public class MyProject2 {
+
+    public static void main(String[] args) {
+        String nama = "MUADZ";
+        System.out.println(nama.substring(0, 3));
+    }
+}
